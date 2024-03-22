bab4d8e5c4cea58f2f80ffc0ff8cecb9a3dc507a
Valentin Gaidarov <valentin.gaidarov@gmail.com>
2024-03-22 17:20:58 +0200

    responsive switched from common to file

--- Client/src/app/core/footer/footer.component.ts
+++ Client/src/app/core/footer/footer.component.ts
@@ -7,23 +7,20 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class FooterComponent implements OnInit, OnDestroy {
     showFooter: boolean = false;
-    timeoutId!: ReturnType<typeof setTimeout> | undefined; // Initialize it as undefined
+    timeoutId!: ReturnType<typeof setTimeout> | undefined;
 
     constructor() {}
 
     ngOnInit() {
-        // Make sure setTimeout is called only once
         if (!this.timeoutId) {
-            // Simulate a delay before showing the footer
             this.timeoutId = setTimeout(() => {
               console.log(this.showFooter)
                 this.showFooter = true;
-            }, 100); // Adjust the delay time as needed (in milliseconds)
+            }, 100);
         }
     }
 
     ngOnDestroy() {
-        // Clear the timeout when the component is destroyed
         console.log(this.showFooter);
         this.showFooter = false;
         if (this.timeoutId) {
