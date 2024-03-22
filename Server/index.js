31cabd26001a31ef56ef14b8c43717c05aa3b432
Valentin Gaidarov <valentin.gaidarov@gmail.com>
2024-03-23 01:53:48 +0200

    cookie-parser added

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true
+  }));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.json({
