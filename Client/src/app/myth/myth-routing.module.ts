0e92615afd19f183279fe065c95d59291526b756
Valentin Gaidarov <valentin.gaidarov@gmail.com>
2024-03-28 21:58:59 +0200

    routing for liking and deleting added

--- Client/src/app/myth/myth-routing.module.ts
+++ Client/src/app/myth/myth-routing.module.ts
@@ -5,6 +5,8 @@ import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
 import { CurrentMythComponent } from './current-myth/current-myth.component';
 import { AuthActivate } from '../guard/auth.activate';
+import { DeleteComponent } from './delete/delete.component';
+import { LikeComponent } from './like/like.component';
 
 
 const routes: Routes = [
@@ -13,6 +15,9 @@ const routes: Routes = [
     { path:'create', component: CreateComponent, canActivate: [AuthActivate] },
     { path:':mythId/edit', component: EditComponent, canActivate: [AuthActivate] },
     { path: ':mythId/details', component: CurrentMythComponent, pathMatch: 'full', },
+    
+    { path: ':mythId/delete', component: DeleteComponent, pathMatch: 'full', },
+    { path: ':mythId/like', component: LikeComponent, pathMatch: 'full', },
   ]}
 ]
 
